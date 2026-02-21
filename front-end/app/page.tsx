aca84e5f7c70c49890b0a3f0344ca85680783e6f
iya francesca <ifmonterola2004@gmail.com>
2026-02-20 20:59:09 -0600

    Edit Styling

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="relative flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
       <h1 className="page-title text-3xl sm:text-4xl">
-        <span className="title">QDoc</span>
+        <span className="title">Vaccine Eligibility</span>
       </h1>
        <main className="floating-card">
           <h1 className="text-3xl font-bold text-black">Login</h1>
