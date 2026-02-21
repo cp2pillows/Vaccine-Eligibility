3942c1d502153511019e49533c68c6cc398b2744
Ty Paragas <ty.paragas01@gmail.com>
2026-02-21 10:23:01 -0600

    Change login to user info

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,30 +1,78 @@
-import Image from "next/image";
+'use client';
+
+import { useState } from "react";
 
 export default function Home() {
+  const [formData, setFormData] = useState({
+    dateOfBirth: "",
+  });
+
+  function handleChange(
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  async function handleSubmit(e: React.FormEvent)
+  {
+    e.preventDefault();
+
+    try
+    {
+      const response = await fetch("http://localhost:3000/user", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+        credentials: 'include',
+      });
+
+      if (!response.ok)
+      {
+        throw new Error("Failed to submit");
+      }
+
+      setFormData({
+        dateOfBirth: "",
+      });
+    }
+    catch(error) 
+    {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="relative flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
       <h1 className="page-title text-3xl sm:text-4xl">
         <span className="title">Vaccine Eligibility</span>
       </h1>
        <main className="floating-card">
-          <h1 className="text-3xl font-bold text-black">Login</h1>
-        <div className="w-full flex flex-col gap-4 mt-8">
-          <input placeholder="Username" className="bg-white text-black"/>
-          <input
-            placeholder="Password"
-            className="bg-white text-black"
-            type="password"
-          />
-          <div className="w-full flex justify-center">
-            <a
-              type="button"
-              href="/questions"
-              className="bubble-button"
-            >
-              Login
-            </a>
+          <h1 className="text-3xl font-bold text-black">Enter your date of birth</h1>
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div className="w-full flex flex-col gap-4 mt-8">
+              <input
+                name="dateOfBirth"
+                type="date"
+                value={formData.dateOfBirth}
+                onChange={handleChange}
+                required
+                className="bg-white text-black"
+              />
+              <div className="w-full flex justify-center">
+                <button
+                  type="submit"
+                  className="bubble-button"
+                >
+                  Submit
+                </button>
+              </div>
           </div>
-        </div>
+        </form>
       </main>
     </div>
   );
